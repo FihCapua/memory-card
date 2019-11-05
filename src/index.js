bb57532062c7e7c9be17728daab78bd4beee5d7d
FihCapua <fihcapua@gmail.com>
2019-11-05 10:43:56 -0300

    [021] - Desafio - incluir outro elemento de carta com CSS e JS

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
 
 // Uso de Template String
-const $icon = `
-    <img 
-        class='icon' 
-        src='img/icon-collabcode.png' 
-        alt='Gueio'
-    >
-`;
+const $icon = ` <img  class='icon' 
+                    src='img/icon-collabcode.png'  alt='Gueio'>
+                `;
+
+
+const $iconC = `<div class="icon-c">
+                    <img class='memory-card' src='img/icon-c.png' alt='Gueio'>
+                </div>`
 
 $memoryCard.classList.add("memory-card");
 
 $root.insertBefore($memoryCard, null);
 $memoryCard.insertAdjacentHTML("beforeend",$icon);
+$memoryCard.insertAdjacentHTML("beforeend",$iconC);
