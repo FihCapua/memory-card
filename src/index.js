ea47a8d1a3d4878b1faeb21babe0a8947e0b6a94
FihCapua <fihcapua@gmail.com>
2019-11-10 19:36:12 -0300

    [021] - Correção de incluir outro elemento de carta com CSS e JS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $memoryCardFront = document.createElement("article");
 
 // Uso de Template String
-const $icon = `
-    <img 
-        class='icon' 
-        src='img/icon-collabcode.png' 
-        alt='Gueio'
-    >
-`;
+const $iconCollab = ` <img  class='icon' 
+                    src='img/icon-collabcode.png'  alt='Gueio'>
+                `;
+
+const $iconC = ` <img  class='icon' 
+                    src='img/icon-c.png'  alt='Livro da linguagem C++'>
+                `;
 
 $memoryCard.classList.add("memory-card");
-
 $root.insertBefore($memoryCard, null);
-$memoryCard.insertAdjacentHTML("beforeend",$icon);
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, $memoryCard);
+
+$memoryCard.insertAdjacentHTML("afterbegin",$iconCollab);
+$memoryCardFront.insertAdjacentHTML("afterbegin",$iconC);
